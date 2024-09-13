60d2b5ae15b097bea4712a66107447c11bf19cb3
JPMartins <jpmartins@nelogica.com.br>
2024-09-13 13:26:15 -0300

    feat: Prisma & MongoDB Connection + Category Filter

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,10 +1,16 @@
+import Categories from '@/components/Categories'
 import SearchInput from '@/components/SearchInput'
+import db from '@/lib/prismadb'
 import React from 'react'
 
-const RootPage = () => {
+const RootPage = async () => {
+
+  const categories = await db.category.findMany()
+
   return (
     <div className='h-full p-4 space-y-2'>
       <SearchInput />
+      <Categories data={categories} />
     </div>
   )
 }
